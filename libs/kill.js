60f3ea03464308772152e82bef6ee32274b77e6b
Guo Yu <i@guoyu.me>
2015-02-01 16:58:23 +0800

    Improve behavior when require module

--- libs/kill.js
+++ libs/kill.js
@@ -2,15 +2,23 @@ var exeq = require('exeq');
 
 module.exports = kill;
 
-function kill(port) {
+function kill(port, callback) {
   var p = port || 3000;
   var killInMacOSX = "lsof -i tcp:" + p + " | grep LISTEN | awk '{print $2}' | xargs kill -9 ";
 
   exeq(killInMacOSX)
   .then(function(){
-    goodbye(p);
+    if (!module.parent)
+      return goodbye(p)
+
+    if (callback && typeof(callback) === 'function')
+      callback(null, true);
   }).catch(function(err){
-    throw err;
+    if (!module.parent)
+      throw err;
+
+    if (callback && typeof(callback) === 'function')
+      callback(err);
   });
 }
 
